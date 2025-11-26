359c19ef09f856088a7430fdd09055c2fe1242a2
nagarajansubbiah25 <nagarajan.subbiah@defra.gov.uk>
2025-11-26 11:32:39 +0000

    remove the unused folder

--- trade-ipaff-ops-automation-tests/Defra.Trade.MSD365.SpecFlowBindings/Tables/WorkOrderServiceTaskTable.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Defra.Trade.Plants.SpecFlowBindings.Tables;
-
-public class WorkOrderServiceTaskTable
-{
-    public string TaskType { get; set; }
-    public string StatusReason { get; set; }
-    public string Complete { get; set; }
-}
