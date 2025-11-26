96129b20478bbdc78d2d968c98a4f0b5843f1c11
nagarajansubbiah25 <nagarajan.subbiah@defra.gov.uk>
2025-11-26 11:27:00 +0000

    init (#1)

---
+++ trade-ipaff-ops-automation-tests/Defra.UI.Tests/Hooks/PageHooks.cs
@@ -0,0 +1,49 @@
+﻿using Defra.UI.Tests.Configuration;
+using Defra.UI.Tests.Data.Users;
+using Defra.UI.Tests.HelperMethods;
+using Defra.UI.Tests.Pages.Classes;
+using Defra.UI.Tests.Pages.Interfaces;
+using Defra.UI.Tests.Tools;
+using Reqnroll;
+using Reqnroll.BoDi;
+
+namespace Defra.UI.Tests.Hooks
+{
+    [Binding]
+    public class PageHooks
+    {
+
+        private readonly IObjectContainer _objectContainer;
+        private readonly ScenarioContext _scenarioContext;
+
+        public PageHooks(IObjectContainer objectContainer, ScenarioContext senarioContext)
+        {
+            _objectContainer = objectContainer;
+            _scenarioContext = senarioContext;
+        }
+
+        [BeforeScenario(Order = (int)HookRunOrder.Pages)]
+        public void BeforeScenario()
+        {
+              BindAllPages();
+        }
+
+        private void BindAllPages()
+        {
+            _objectContainer.RegisterInstanceAs(GetBaseWithContainer<UserObject, IUserObject>());
+            _objectContainer.RegisterInstanceAs(GetBaseWithContainer<UrlBuilder, IUrlBuilder>());
+            _objectContainer.RegisterInstanceAs(GetBaseWithContainer<SignInPage, ISignInPage>());
+            _objectContainer.RegisterInstanceAs(GetBaseWithContainer<HomePage, IHomePage>());
+            _objectContainer.RegisterInstanceAs(GetBaseWithContainer<EmailSignUpPage, IEmailSignUpPage>());
+            _objectContainer.RegisterInstanceAs(GetBaseWithContainer<GovernmentGatewayTypePage, IGovernmentGatewayTypePage>());
+            
+            //Read Email
+            _objectContainer.RegisterInstanceAs(GetBaseWithScenarioContext<FetchCodeFromEmail, IFetchCodeFromEmail>());
+        }
+
+
+        private TU GetBaseWithContainer<T, TU>() where T : TU => (TU)Activator.CreateInstance(typeof(T), _objectContainer);
+        private TU GetBaseWithContainerScenarioContext<T, TU>() where T : TU => (TU)Activator.CreateInstance(typeof(T), _objectContainer, _scenarioContext);
+        private TU GetBaseWithScenarioContext<T, TU>() where T : TU => (TU)Activator.CreateInstance(typeof(T), _scenarioContext);
+    }
+}
