2e1c9074f4f1c7d2531147186bf496b86893d246
Vinotha Thiyagarajan <Vinotha.Thiyagarajan@defra.gov.uk>
2025-12-10 15:58:44 +0000

    Implemented the review comment

--- Defra.UI.Tests/Steps/PortOfEntrySteps.cs
+++ Defra.UI.Tests/Steps/PortOfEntrySteps.cs
@@ -37,16 +37,16 @@ namespace Defra.UI.Tests.Steps.IPAFF
         [When("the user populates the transport details {string} {string} {string} {string} {string}")]
         public void WhenTheUserPopulatesTheTransportDetails(string port, string option, string mode, string transId, string DocumentRef)
         {
-            DateTime futureDate = DateTime.Now.AddDays(5);
-            string day = futureDate.Day.ToString();
-            string month = futureDate.Month.ToString();
-            string year = futureDate.Year.ToString();
-            string formattedDate = futureDate.ToString("dd MMM yyyy");
+            var futureDate = DateTime.Now.AddDays(5);
+            var day = futureDate.Day.ToString();
+            var month = futureDate.Month.ToString();
+            var year = futureDate.Year.ToString();
+            var formattedDate = futureDate.ToString("dd MMM yyyy");
 
-            string hour = "23";
-            string minutes = "30";
-            string journeyTimeHours = "8"; // Default journey time for CHED-A
-            string formattedTime = hour + ":" + minutes;
+            var journeyTimeHours = "8"; // Default journey time for CHED-A
+            var formattedTime = futureDate.ToString("HH:mm");
+            var hour = futureDate.Hour.ToString();
+            var minutes = futureDate.Minute.ToString();
 
             portOfEntryPage?.EnterPortOfEntry(port);
             portOfEntryPage?.SelectMeansOfTransport(mode);
