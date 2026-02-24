d88f76bfff1a4e6476aaa46b4e955a611b77a2d0
harsandh <111275363+harsandh@users.noreply.github.com>
2026-02-24 11:27:19 +0000

    SPS - 6944 -> dev (#57)
    * Added scenario SPS-6944
    
    * Added manage certificate validation
    
    * Added user verifications to Add Catch Certificate details page
    
    * Added steps for SPS-6944 Add catch certificate details
    
    * Updated steps for Add Catch Certificate details
    
    * Implemented further steps for SPS-6944
    
    * Completed steps for SPS-6944
    
    * Reverted settings files
    
    * Added step to CHEDP test
    
    * Fixed PR comments
    
    * Fixed PR comments

--- Defra.UI.Tests/Pages/Interfaces/IManageCatchCertificatesPage.cs
+++ Defra.UI.Tests/Pages/Interfaces/IManageCatchCertificatesPage.cs
@@ -6,5 +6,14 @@
         void SelectOption(string option);
         bool VerifyNumberOfCertificates(string numberOfCertificates);
         void ClickAddDetailsLink();
+        bool VerifySuccessMessageDisplaysNumberOfFilesAdded(int expectedFileCount);
+        bool VerifyNumberOfAttachmentsMissingDetails(int expectedMissingCount);
+        bool VerifyCatchCertificatesUploadedHeadingDisplayed();
+        bool VerifyNumberOfCatchCertificatesDisplayedForEachAttachment(int expectedAttachmentCount);
+        bool VerifyEachAttachmentDisplayedAsXofYFormat(int totalAttachments);
+        bool VerifyEachAttachmentHasPopulatedInputBox(int expectedAttachmentCount);
+        bool VerifyEachAttachmentHasUpdateButton(int expectedAttachmentCount);
+        bool VerifyEachAttachmentHasAddDetailsAndRemoveLinks(int expectedAttachmentCount);
+        void ClickViewOrAmendDetailsLinkForAttachment(int attachmentNumber);
     }
 }
