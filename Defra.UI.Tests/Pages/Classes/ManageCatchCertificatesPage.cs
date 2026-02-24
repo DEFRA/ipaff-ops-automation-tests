d26fbde8352f825c3364879fd06a6f2817ee2079
harsandh <harbir.sandhu@capgemini.com>
2026-02-24 10:47:03 +0000

    Fixed PR comments

--- Defra.UI.Tests/Pages/Classes/ManageCatchCertificatesPage.cs
+++ Defra.UI.Tests/Pages/Classes/ManageCatchCertificatesPage.cs
@@ -14,19 +14,21 @@ namespace Defra.UI.Tests.Pages.Classes
         private IWebElement rdoMoreCertificateOption(string option) => _driver.FindElement(By.XPath($"//input[@type='radio' and @value='{option}']"));
         private IReadOnlyCollection<IWebElement> RemoveCertificates => _driver.WaitForElements(By.XPath("//a[normalize-space()='Remove']"));
         private IWebElement lnkAddDetails => _driver.FindElement(By.LinkText("Add details"));
-        private By successBannerBy => By.Id("upload-success-banner");
-        private By successBannerContentBy => By.XPath("//div[@id='upload-success-banner']//p[@class='govuk-body']");
-        private By missingDocumentsBannerBy => By.Id("missing-documents-info-banner");
-        private By missingDocumentsTitleBy => By.Id("missing-documents-title");
-        private By missingDocumentsListBy => By.Id("missing-documents-list");
-        private By catchCertificatesUploadedHeadingBy => By.XPath("//h2[contains(text(), 'Catch certificates uploaded')]");
-        private By attachmentBlocksBy => By.CssSelector("div.attachment-block");
-        private By attachmentCaptionsBy => By.XPath("//span[contains(@class, 'govuk-caption-m')]");
-        private By certificateInputBoxesBy => By.XPath("//input[contains(@id, 'number-of-catch-certificates-')]");
-        private By updateButtonsBy => By.XPath("//button[contains(@id, 'update-catch-certificates-')]");
-        private By addDetailsLinksBy => By.XPath("//a[contains(@id, 'add-') and contains(text(), 'Add details')]");
-        private By removeLinksBy => By.XPath("//a[contains(@id, 'remove-') and contains(text(), 'Remove')]");
-        private By numberOfCertificatesLabelsBy => By.XPath("//label[contains(text(), 'Number of catch certificates in this attachment')]");
+        private IWebElement successBanner => _driver.FindElement(By.Id("upload-success-banner"));
+        private IWebElement successBannerContent => _driver.FindElement(By.XPath("//div[@id='upload-success-banner']//p[@class='govuk-body']"));
+        private IWebElement missingDocumentsBanner => _driver.FindElement(By.Id("missing-documents-info-banner"));
+        private IWebElement missingDocumentsTitle => _driver.FindElement(By.Id("missing-documents-title"));
+        private IWebElement missingDocumentsList => _driver.FindElement(By.Id("missing-documents-list"));
+        private IWebElement catchCertificatesUploadedHeading => _driver.FindElement(By.XPath("//h2[contains(text(), 'Catch certificates uploaded')]"));
+        private IReadOnlyCollection<IWebElement> attachmentBlocks => _driver.FindElements(By.CssSelector("div.attachment-block"));
+        private IReadOnlyCollection<IWebElement> attachmentCaptions => _driver.FindElements(By.XPath("//span[contains(@class, 'govuk-caption-m')]"));
+        private IReadOnlyCollection<IWebElement> certificateInputBoxes => _driver.FindElements(By.XPath("//input[contains(@id, 'number-of-catch-certificates-')]"));
+        private IReadOnlyCollection<IWebElement> updateButtons => _driver.FindElements(By.XPath("//button[contains(@id, 'update-catch-certificates-')]"));
+        private IReadOnlyCollection<IWebElement> addDetailsLinks => _driver.FindElements(By.XPath("//a[contains(@id, 'add-') and contains(text(), 'Add details')]"));
+        private IReadOnlyCollection<IWebElement> removeLinks => _driver.FindElements(By.XPath("//a[contains(@id, 'remove-') and contains(text(), 'Remove')]"));
+        private IReadOnlyCollection<IWebElement> numberOfCertificatesLabels => _driver.FindElements(By.XPath("//label[contains(text(), 'Number of catch certificates in this attachment')]"));
+        private By missingDocumentsListItemsBy => By.TagName("li");
+        private By viewAmendDetailsLinkBy => By.XPath(".//a[contains(text(), 'View or amend details')]");
         #endregion
 
         private IWebDriver _driver => _objectContainer.Resolve<IWebDriver>();
@@ -55,83 +57,51 @@ namespace Defra.UI.Tests.Pages.Classes
 
         public bool VerifySuccessMessageDisplaysNumberOfFilesAdded(int expectedFileCount)
         {
-            var successBanner = _driver.FindElement(successBannerBy);
-            if (!successBanner.Displayed) return false;
-
-            var successContent = _driver.FindElement(successBannerContentBy);
-            var text = successContent.Text.Trim();
-
-            return text.Contains($"{expectedFileCount} documents uploaded");
+            return successBanner.Displayed &&
+                   successBannerContent.Text.Trim().Contains($"{expectedFileCount} documents uploaded");
         }
 
         public bool VerifyNumberOfAttachmentsMissingDetails(int expectedMissingCount)
         {
-            var missingBanner = _driver.FindElement(missingDocumentsBannerBy);
-            if (!missingBanner.Displayed) return false;
+            var listItems = missingDocumentsList.FindElements(missingDocumentsListItemsBy);
 
-            var missingTitle = _driver.FindElement(missingDocumentsTitleBy);
-            var titleText = missingTitle.Text.Trim();
-
-            var missingList = _driver.FindElement(missingDocumentsListBy);
-            var listItems = missingList.FindElements(By.TagName("li"));
-
-            return titleText.Contains($"{expectedMissingCount} attachments are missing details") &&
+            return missingDocumentsBanner.Displayed &&
+                   missingDocumentsTitle.Text.Trim().Contains($"{expectedMissingCount} attachments are missing details") &&
                    listItems.Count == expectedMissingCount;
         }
 
         public bool VerifyCatchCertificatesUploadedHeadingDisplayed()
         {
-            var heading = _driver.FindElement(catchCertificatesUploadedHeadingBy);
-            return heading.Displayed &&
-                   heading.Text.Trim().Equals("Catch certificates uploaded", StringComparison.OrdinalIgnoreCase);
+            return catchCertificatesUploadedHeading.Displayed &&
+                   catchCertificatesUploadedHeading.Text.Trim().Equals("Catch certificates uploaded", StringComparison.OrdinalIgnoreCase);
         }
 
         public bool VerifyNumberOfCatchCertificatesDisplayedForEachAttachment(int expectedAttachmentCount)
         {
-            var labels = _driver.FindElements(numberOfCertificatesLabelsBy);
-            var attachmentBlocks = _driver.FindElements(attachmentBlocksBy);
-
-            return labels.Count == expectedAttachmentCount &&
+            return numberOfCertificatesLabels.Count == expectedAttachmentCount &&
                    attachmentBlocks.Count == expectedAttachmentCount &&
-                   labels.All(label => label.Displayed &&
-                              label.Text.Contains("Number of catch certificates in this attachment"));
+                   numberOfCertificatesLabels.All(label => label.Displayed &&
+                       label.Text.Contains("Number of catch certificates in this attachment"));
         }
 
         public bool VerifyEachAttachmentDisplayedAsXofYFormat(int totalAttachments)
         {
-            var captions = _driver.FindElements(attachmentCaptionsBy);
-
-            if (captions.Count != totalAttachments)
-            {
-                return false;
-            }
-
-            for (int i = 0; i < totalAttachments; i++)
-            {
-                var expectedText = $"Attachment {i + 1} of {totalAttachments}";
-                if (!captions[i].Text.Trim().Equals(expectedText, StringComparison.OrdinalIgnoreCase))
-                {
-                    return false;
-                }
-            }
-
-            return true;
+            return attachmentCaptions.Count == totalAttachments &&
+                   attachmentCaptions
+                       .Select((caption, i) => new { caption, i })
+                       .All(x => x.caption.Text.Trim().Equals($"Attachment {x.i + 1} of {totalAttachments}", StringComparison.OrdinalIgnoreCase));
         }
 
         public bool VerifyEachAttachmentHasPopulatedInputBox(int expectedAttachmentCount)
         {
-            var inputBoxes = _driver.FindElements(certificateInputBoxesBy);
-
-            return inputBoxes.Count == expectedAttachmentCount &&
-                   inputBoxes.All(input =>
+            return certificateInputBoxes.Count == expectedAttachmentCount &&
+                   certificateInputBoxes.All(input =>
                        input.Displayed &&
                        !string.IsNullOrWhiteSpace(input.GetAttribute("value")));
         }
 
         public bool VerifyEachAttachmentHasUpdateButton(int expectedAttachmentCount)
         {
-            var updateButtons = _driver.FindElements(updateButtonsBy);
-
             return updateButtons.Count == expectedAttachmentCount &&
                    updateButtons.All(btn => btn.Displayed &&
                                             btn.Text.Trim().Equals("Update", StringComparison.OrdinalIgnoreCase));
@@ -139,33 +109,22 @@ namespace Defra.UI.Tests.Pages.Classes
 
         public bool VerifyEachAttachmentHasAddDetailsAndRemoveLinks(int expectedAttachmentCount)
         {
-            var addDetailsLinks = _driver.FindElements(addDetailsLinksBy);
-            var removeLinks = _driver.FindElements(removeLinksBy);
-
-            var addDetailsValid = addDetailsLinks.Count == expectedAttachmentCount &&
-                                 addDetailsLinks.All(link => link.Displayed &&
-                                                            link.Text.Contains("Add details"));
-
-            var removeLinksValid = removeLinks.Count == expectedAttachmentCount &&
-                                  removeLinks.All(link => link.Displayed &&
-                                                         link.Text.Contains("Remove"));
-
-            return addDetailsValid && removeLinksValid;
+            return addDetailsLinks.Count == expectedAttachmentCount &&
+                   addDetailsLinks.All(link => link.Displayed && link.Text.Contains("Add details")) &&
+                   removeLinks.Count == expectedAttachmentCount &&
+                   removeLinks.All(link => link.Displayed && link.Text.Contains("Remove"));
         }
 
         public void ClickViewOrAmendDetailsLinkForAttachment(int attachmentNumber)
         {
-            var attachmentBlocks = _driver.FindElements(attachmentBlocksBy);
-
             if (attachmentNumber < 1 || attachmentNumber > attachmentBlocks.Count)
             {
                 throw new ArgumentOutOfRangeException(nameof(attachmentNumber),
                     $"Attachment number {attachmentNumber} is out of range. Available attachments: {attachmentBlocks.Count}");
             }
 
-            var targetBlock = attachmentBlocks[attachmentNumber - 1];
-            var viewAmendLink = targetBlock.FindElement(By.XPath(".//a[contains(text(), 'View or amend details')]"));
-            viewAmendLink.Click();
+            var targetBlock = attachmentBlocks.ElementAt(attachmentNumber - 1);
+            targetBlock.FindElement(viewAmendDetailsLinkBy).Click();
         }
     }
 }
